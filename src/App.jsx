9c454baca0181545d15ab44525db8d74fd814539
LeodanLVL <leodanlazo00@gmail.com>
2024-07-30 17:46:57 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import logo from './assets/Icons/logo.png';
+
+function App() {
+  
+  return (
+    <main className='main'>
+
+    <div className='nav_bar menu'>
+        <p>option</p>
+    </div>
+    <div className='mainl'>
+      <img className='main_logo' src={logo} alt="Logo SunRac" />
+      
+    </div>
+    </main>
+  )
+}
+
+export default App
